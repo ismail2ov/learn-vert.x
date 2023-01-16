92cc15a0583b96c9d697a9bb1393e3c77346979e
Ismail Ahmedov <ismail.ahmedov@asesormasmovil.es>
2023-01-16 02:10:25 +0100

    Vert.x Verticle Scaling

--- first-vertx-project/src/main/java/com/github/ismail2ov/verticles/MainVerticle.java
+++ first-vertx-project/src/main/java/com/github/ismail2ov/verticles/MainVerticle.java
@@ -1,6 +1,7 @@
 package com.github.ismail2ov.verticles;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 
@@ -16,6 +17,9 @@ public class MainVerticle extends AbstractVerticle {
     System.out.println("Start " + getClass().getName());
     vertx.deployVerticle(new VerticleA());
     vertx.deployVerticle(new VerticleB());
+    vertx.deployVerticle(VerticleN.class.getName(),
+      new DeploymentOptions().setInstances(4)
+    );
     startPromise.complete();
   }
 }
