922f3aac11d27fac27f5bbb8b2d52f43f66fcf54
Ismail Ahmedov <ismail.ahmedov@asesormasmovil.es>
2023-01-16 01:50:58 +0100

    The Vert.x Object

--- first-vertx-project/src/main/java/com/github/ismail2ov/starter/MainVerticle.java
+++ first-vertx-project/src/main/java/com/github/ismail2ov/starter/MainVerticle.java
@@ -2,15 +2,21 @@ package com.github.ismail2ov.starter;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
 
 public class MainVerticle extends AbstractVerticle {
+
+  public static void main(String[] args) {
+    var vertx = Vertx.vertx();
+    vertx.deployVerticle(new MainVerticle());
+  }
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
     vertx.createHttpServer().requestHandler(req -> {
       req.response()
         .putHeader("content-type", "text/plain")
-        .end("Hello from Vert.x!");
+        .end("Hello World!");
     }).listen(8888, http -> {
       if (http.succeeded()) {
         startPromise.complete();
